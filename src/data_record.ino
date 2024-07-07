f988628e334923f1bf6c9d8fcc097245fec8f678
Orion Grieco <46628700+Orion-Grieco@users.noreply.github.com>
2024-07-07 19:10:13 -0400

    created ino code to run sensor data collection

---
+++ src/data_record.ino
@@ -0,0 +1,45 @@
+/*
+  DHT22 Temperature and humidity sensor
+*/
+
+/*
+  Library import
+*/
+
+#include <DHT.h> //importing DHT sensor library
+// constants
+#define DHTPIN 2      // declaring location of Data-Pin on arduino board
+#define DHTTYPE DHT22 // declaring the type of temp/pressure sensor
+DHT dht = DHT(DHTPIN, DHTTYPE);
+
+// creating variables
+int chk;
+float humidity;    // float used to store values for humidity
+float temperature; // float used to store values for temperature
+
+void setup()
+{
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop()
+{
+  delay(2000);
+
+  // reading data
+  humidity = dht.readHumidity();
+  temperature = dht.readTemperature();
+
+  // returning values
+  // Serial.print("Humidity: ");
+  // Serial.print("\b");
+  Serial.print(humidity);
+  Serial.print("% , ");
+  // Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.print(" °Celsius");
+  Serial.print("\n");
+  delay(1000); // 1000ms, or 1 sec
+  // 3 second TOTAL delay when factoring in initial 2000ms delay
+}
